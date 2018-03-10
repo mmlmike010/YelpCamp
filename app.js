16e3c501494bdee500d158898330aa1c9429d275
Michael Lacsamana <michael.m.lacsamana@gmail.com>
2018-03-10 21:33:53 +0000

    update

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ mongoose.connect("mongodb://localhost/yelp_camp");
 //require seeds file
 var seedDB = require("./seeds");
 seedDB();
+//coments
+var Comment = require("./models/comment");
 
 
 
@@ -24,7 +26,7 @@ app.get("/campgrounds", function(req, res){
             if(err){
                 console.log(err);
             } else{
-                res.render("index", {campgrounds:allCampgrounds});
+                res.render("campgrounds/index", {campgrounds:allCampgrounds});
             }
         })
         //res.render("campgrounds", {campgrounds:campgrounds});
@@ -52,7 +54,7 @@ app.post("/campgrounds", function(req,res){
 
 //NEW- show form to create new campground
 app.get("/campgrounds/new", function(req,res){
-   res.render("new"); 
+   res.render("campgrounds/new"); 
 });
 
 //SHOW - shows more info about one campground
@@ -64,9 +66,47 @@ app.get("/campgrounds/:id", function(req, res){
            console.log(err);
        } else{
                //render show template with that campground
-        res.render("show", {campground: foundCampground});
+        res.render("campgrounds/show", {campground: foundCampground});
        }
     });
+});
+
+// =================================
+// COMMENTS ROUTES
+// =================================
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    //find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("comments/new", {campground: campground});
+        }
+    });
+});
+
+app.post("/campgrounds/:id/comments", function(req,res){
+    //lookup campground using ID
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else{
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                } else{
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            });
+        }
+    });
+    //create new comment
+    //connect new comment to campground
+    //redirect campground show page
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
